a6801decc290f14c8826b37f142ffe5234a07baa
Peizhao Li <peizhaol@linux-30.andrew.cmu.edu>
2022-04-13 23:53:06 -0400

    need to send decision, handle decision, send ack, handle ack, and handle abort

--- mycode/Server.java
+++ mycode/Server.java
@@ -46,13 +46,65 @@ public class Server implements ProjectLib.CommitServing {
             ProjectLib.Message prepareMsg = new ProjectLib.Message(addr, body.serialize());
             pl.sendMessage(prepareMsg);
         }
+    }
+
+    private static class messageHandler implements Runnable {
+        private String srcAddr;
+        private ProjectLib.Message clientMessage;
+
+        public messageHandler(ProjectLib.Message clientMessage) {
+            this.clientMessage = clientMessage;
+        }
+
+        public void run() {
+            srcAddr = clientMessage.addr;
+            MyMessage myMessage = (MyMessage) serializeTool.deserialize(clientMessage.body);
+            if (myMessage.type == 2) {
+                voteHandler(srcAddr, myMessage);
+            } else if (myMessage.type == 4) {
+                ackHandler(srcAddr, myMessage);
+            }
+        }
+    }
+
+    public static void voteHandler(String srcAddr, MyMessage myMessage) {
+        String collageName = myMessage.collageName;
+        if (processMap.containsKey(collageName)) {
+            CommitProcess currentProcess = processMap.get(collageName);
+            boolean voteResult = myMessage.boolResult;
+            currentProcess.voteResult.put(srcAddr, voteResult);
+            if (currentProcess.checkVoted()) {
+                boolean decision = currentProcess.checkVoteResult();
+                sendDecision();
+            }
+        } else {
+            System.err.println("handleVote(): NON-EXISTED COMMIT RECORD, STH WRONG");
+        }
+    }
+
+    public static void sendDecision() {
+        
+    }
+
+    public static void ackHandler(String srcAddr, MyMessage myMessage) {
 
     }
+
+    // public static void messageHandler(ProjectLib.Message clientMessage) {
+        // private String srcAddr = clientMessage.addr;
+        // MyMessage myMessage = (MyMessage) serializeTool.deserialize(clientMessage.body);
+    // }
 
     public static void main(String[] args) {
         int port =  Integer.parseInt(args[0]);
         Server server = new Server();
         pl = new ProjectLib(port, server);
+
+        while (true) {
+            ProjectLib.Message receivedMessage = pl.getMessage();
+            Thread handler = new Thread(new messageHandler(receivedMessage));
+            handler.start();
+        }
         
 
     }
--- mycode/UserNode.java
+++ mycode/UserNode.java
@@ -1,10 +1,67 @@
+import java.util.concurrent.*;
+import java.util.*;
+
 public class UserNode {
 
     private static ProjectLib pl;
+    private static final String serverAddr = "Server";
+    private static ConcurrentHashMap<String, Boolean> lockedFile = new ConcurrentHashMap<String, Boolean>();
+    private static String clientID;
+
+    public static void messageHandler(ProjectLib.Message receivedMessage) {
+        String srcName = receivedMessage.addr;
+        byte[] receivedBytes = receivedMessage.body;
+        MyMessage myMessage = (MyMessage) serializeTool.deserialize(receivedBytes);
+        if (myMessage.type == 1) {
+            prepareHandler(srcName, myMessage);
+        } else if (myMessage.type == 3) {
+            decisionHandler(srcName, myMessage);
+        }
+
+    }
+
+    public static boolean lockFile(MyMessage myMessage) {
+        for (String tmpString: myMessage.sources) {
+            String userName = tmpString.split(":")[0];
+            String fileName = tmpString.split(":")[1];
+            if (userName.equals(clientID)) {
+                // File has already been locked, refuse this commit
+                if (lockedFile.containsKey(fileName)) {
+                    return false;
+                } else {
+                    lockedFile.put(fileName,true);
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void prepareHandler(String srcName, MyMessage myMessage) {
+        boolean voteResult = pl.askUser(myMessage.collageContent, myMessage.sources);
+        MyMessage voteMessage = new MyMessage(2, myMessage.collageName, null, myMessage.sources);
+        assert (srcName == "Server");
+        
+        if (!lockFile(myMessage)) {
+            voteResult = false;
+        }
+
+        voteMessage.boolResult = voteResult;
+        ProjectLib.Message messageToSend = new ProjectLib.Message(serverAddr, serializeTool.serialize(voteMessage));
+        pl.sendMessage(messageToSend);
+    }
+
+    public static void decisionHandler(String srcName, MyMessage myMessage) {
+        
+    }
 
     public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
-        int id = Integer.parseInt(args[1);
-        pl = new ProjectLib(port, id);
+        clientID = args[1];
+        pl = new ProjectLib(port, clientID);
+
+        while (true) {
+            ProjectLib.Message receivedMessage = pl.getMessage();
+            messageHandler(receivedMessage);
+        }
     }
 }
